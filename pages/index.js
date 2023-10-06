c8dccde557b29388db95f79924c5ddbc1bcf95ec
KIRAN KUMAR <kiran.k@armiasystems.com>
2023-10-06 21:17:34 +0530

    Remove last para from auther bio

--- pages/index.js
+++ pages/index.js
@@ -29,10 +29,10 @@ export default function Home({ allPostsData }) {
             Twitter.
           </a>
         </p>
-        <p>
+        {/* <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        </p> */}
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
